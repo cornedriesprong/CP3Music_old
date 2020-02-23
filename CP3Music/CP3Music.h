f582e068d518855b7519db47f7054aaca80ad866
Corné <cdriesprong@gmail.com>
2020-02-23 15:07:37 +0100

    Initial Commit

---
+++ CP3Music/CP3Music.h
@@ -0,0 +1,19 @@
+//
+//  CP3Music.h
+//  CP3Music
+//
+//  Created by Corné on 23/02/2020.
+//  Copyright © 2020 cp3.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CP3Music.
+FOUNDATION_EXPORT double CP3MusicVersionNumber;
+
+//! Project version string for CP3Music.
+FOUNDATION_EXPORT const unsigned char CP3MusicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CP3Music/PublicHeader.h>
+
+
