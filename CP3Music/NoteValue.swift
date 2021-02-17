b96c780f8ebbd0e238b4bb9f962e6d5f7783ae41
Corné <cdriesprong@gmail.com>
2021-02-17 13:36:47 +0100

    fixes in note values

--- CP3Music/NoteValue.swift
+++ CP3Music/NoteValue.swift
@@ -13,7 +13,10 @@ public enum NoteValue: Int, CaseIterable {
     // define a fake class to reference the bundle to, for showing images
     class SpoofClass {}
     
+    case doubleWholeNote
+    case dottedWholeNote
     case wholeNote
+    case wholeNoteTriplet
     case dottedHalfNote
     case halfNote
     case halfNoteTriplet
@@ -26,43 +29,78 @@ public enum NoteValue: Int, CaseIterable {
     case dottedSixteenthNote
     case sixteenthNote
     case sixteenthNoteTriplet
+    case dottedThirthySecondNote
+    case thirthySecondNote
+    case thirthySecondNoteTriplet
+    case dottedSixtyFourthNote
+    case sixtyFourthNote
+    case sixtyFourthNoteTriplet
     
-    public var clockDivision: Float {
+    public var stepDuration: Float {
+        
+        let value: Float
+        
         switch self {
+        case .doubleWholeNote:
+            value = NoteValue.wholeNote.stepDuration * 2.0
+        case .dottedWholeNote:
+            value = NoteValue.wholeNote.stepDuration * 1.5
         case .wholeNote:
-            return 1.0
+            value = 1.0
+        case .wholeNoteTriplet:
+            value = NoteValue.doubleWholeNote.stepDuration / 3.0
         case .dottedHalfNote:
-            return 1.0 / 1.5
+            value = NoteValue.halfNote.stepDuration * 1.5
         case .halfNote:
-            return 1.0 / 2.0
+            value = NoteValue.wholeNote.stepDuration / 2.0
         case .halfNoteTriplet:
-            return 1.0 / 3.0
+            value = NoteValue.wholeNote.stepDuration / 3.0
         case .dottedQuarterNote:
-            return 1.0 / 4.0
+            value = NoteValue.quarterNote.stepDuration * 1.5
         case .quarterNote:
-            return 1.0 / 4.0
+            value = NoteValue.wholeNote.stepDuration / 4.0
         case .quarterNoteTriplet:
-            return 1.0 / 6.0
+            value = NoteValue.halfNote.stepDuration / 3.0
         case .dottedEighthNote:
-            return 1.0 / 7.0
+            value = NoteValue.eighthNote.stepDuration * 1.5
         case .eighthNote:
-            return 1.0 / 8.0
+            value = NoteValue.wholeNote.stepDuration / 8.0
         case .eighthNoteTriplet:
-            return 1.0 / 10.0
+            value = NoteValue.quarterNote.stepDuration / 3.0
         case .dottedSixteenthNote:
-            return 1.0 / 16.0
+            value = NoteValue.sixteenthNote.stepDuration * 1.5
         case .sixteenthNote:
-            return 1.0 / 16.0
+            value = NoteValue.wholeNote.stepDuration / 16.0
         case .sixteenthNoteTriplet:
-            return 1.0 / 20.0
+            value = NoteValue.eighthNote.stepDuration / 3.0
+        case .dottedThirthySecondNote:
+            value = NoteValue.thirthySecondNote.stepDuration * 1.5
+        case .thirthySecondNote:
+            value = NoteValue.wholeNote.stepDuration / 32.0
+        case .thirthySecondNoteTriplet:
+            value = NoteValue.sixteenthNote.stepDuration / 3.0
+        case .dottedSixtyFourthNote:
+            value = NoteValue.sixtyFourthNote.stepDuration * 1.5
+        case .sixtyFourthNote:
+            value = NoteValue.wholeNote.stepDuration / 64.0
+        case .sixtyFourthNoteTriplet:
+            value = NoteValue.thirthySecondNote.stepDuration / 3.0
         }
+        
+        return value * 16.0
     }
     
     public var image: UIImage? {
         let bundle = Bundle(for: SpoofClass.self)
         switch self {
+        case .doubleWholeNote:
+            return UIImage(named: "double_whole_note", in: bundle, compatibleWith: nil)
+        case .dottedWholeNote:
+            return UIImage(named: "whole_note_dotted", in: bundle, compatibleWith: nil)
         case .wholeNote:
             return UIImage(named: "whole_note", in: bundle, compatibleWith: nil)
+        case .wholeNoteTriplet:
+            return UIImage(named: "whole_note_triplet", in: bundle, compatibleWith: nil)
         case .dottedHalfNote:
             return UIImage(named: "half_note_dotted", in: bundle, compatibleWith: nil)
         case .halfNote:
@@ -87,6 +125,70 @@ public enum NoteValue: Int, CaseIterable {
             return UIImage(named: "sixteenth_note", in: bundle, compatibleWith: nil)
         case .sixteenthNoteTriplet:
             return UIImage(named: "sixteenth_note_triplet", in: bundle, compatibleWith: nil)
+        case .dottedThirthySecondNote:
+            return UIImage(named: "32nd_note_dotted", in: bundle, compatibleWith: nil)
+        case .thirthySecondNote:
+            return UIImage(named: "32nd_note", in: bundle, compatibleWith: nil)
+        case .thirthySecondNoteTriplet:
+            return UIImage(named: "32nd_note_triplet", in: bundle, compatibleWith: nil)
+        case .dottedSixtyFourthNote:
+            return UIImage(named: "64th_note_dotted", in: bundle, compatibleWith: nil)
+        case .sixtyFourthNote:
+            return UIImage(named: "64th_note", in: bundle, compatibleWith: nil)
+        case .sixtyFourthNoteTriplet:
+            return UIImage(named: "64th_note_triplet", in: bundle, compatibleWith: nil)
+        }
+    }
+}
+
+extension NoteValue: CustomStringConvertible {
+    
+    public var description: String {
+        switch self {
+        case .doubleWholeNote:
+            return "double whole note"
+        case .dottedWholeNote:
+            return "dotted whole note"
+        case .wholeNoteTriplet:
+            return "whole note triplet"
+        case .wholeNote:
+            return "whole note"
+        case .dottedHalfNote:
+            return "dotted half note"
+        case .halfNote:
+            return "half note"
+        case .halfNoteTriplet:
+            return "half note triplet"
+        case .dottedQuarterNote:
+            return "dotted quarter note"
+        case .quarterNote:
+            return "quarter note"
+        case .quarterNoteTriplet:
+            return "quarter note triplet"
+        case .dottedEighthNote:
+            return "dotted eighth note"
+        case .eighthNote:
+            return "eighth note"
+        case .eighthNoteTriplet:
+            return "eighth note triplet"
+        case .dottedSixteenthNote:
+            return "dotted 16th note"
+        case .sixteenthNote:
+            return "16th note"
+        case .sixteenthNoteTriplet:
+            return "16th note triplet"
+        case .dottedThirthySecondNote:
+            return "dotted 32nd note"
+        case .thirthySecondNote:
+            return "32nd note"
+        case .thirthySecondNoteTriplet:
+            return "32nd note triplet"
+        case .dottedSixtyFourthNote:
+            return "dotted 64th note"
+        case .sixtyFourthNote:
+            return "64th note"
+        case .sixtyFourthNoteTriplet:
+            return "64th note triplet"
         }
     }
 }
